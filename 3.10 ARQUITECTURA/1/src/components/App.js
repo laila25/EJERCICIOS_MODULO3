6aa6e6ae72a40f0ba2738b186d358db040cae8e4
laila25 <laila2519@gmail.com>
2019-09-16 18:12:27 +0200

    finish 3.10

---
+++ 3.10 ARQUITECTURA/1/src/components/App.js
@@ -0,0 +1,105 @@
+import React from "react";
+import "../stylesheets/App.css";
+import Item from "./item";
+import SearchByCity from "./SearchByCity";
+import SearchByGenre from "./SearchByGenre";
+
+let usersData = [];
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      users: []
+    };
+    this.getDataFromServer = this.getDataFromServer.bind(this);
+    this.filterByCity = this.filterByCity.bind(this);
+    this.filterByGenre = this.filterByGenre.bind(this);
+    this.desFilterByGenre = this.desFilterByGenre.bind(this);
+    this.getDataFromServer();
+  }
+
+  getDataFromServer() {
+    fetch("https://randomuser.me/api/?results=12")
+      .then(Response => Response.json())
+      .then(data => {
+        usersData = data.results;
+        this.setState({
+          users: usersData
+        });
+        console.log(usersData);
+      });
+  }
+
+  filterByCity(ev) {
+    const citySelected = ev.target.value;
+    if (ev.target.checked === true) {
+      const usersFilterByCity = this.state.users.filter(
+        user => user.location.city === citySelected
+      );
+      this.setState({
+        users: usersFilterByCity
+      });
+    } else {
+      this.setState({
+        users: usersData
+      });
+    }
+  }
+
+  filterByGenre(ev) {
+    const genreSelected = ev.target.value;
+    if (ev.target.checked === true) {
+      const usersFilterByGenre = this.state.users.filter(
+        user => user.gender === genreSelected
+      );
+      this.setState({
+        users: usersFilterByGenre
+      });
+    } else {
+      this.setState({
+        users: usersData
+      });
+    }
+  }
+
+  desFilterByGenre() {
+    this.setState({
+      users: usersData
+    });
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <div className="filters">
+          <p>
+            <b>FILTERS</b>
+          </p>
+          <form>
+            <p>
+              <b>Ciudad</b>
+            </p>
+            <SearchByCity users={usersData} filterByCity={this.filterByCity} />
+            <p>
+              <b>Género:</b>
+            </p>
+            <SearchByGenre
+              users={this.state.users}
+              filterByGenre={this.filterByGenre}
+              desFilterByGenre={this.desFilterByGenre}
+            />
+          </form>
+        </div>
+        <div className="users">
+          <p>USERS</p>
+          <ul>
+            <Item users={this.state.users} />
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ 3.10 ARQUITECTURA/1/src/components/item.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+let usersList;
+
+const Item = props => {
+  let users = props.users;
+  usersList = users.map((user, index) => (
+    <li className="item" key={index}>
+      <div className="itemContent">
+        <img src={user.picture.medium} alt={user.name.first}></img>
+        <div className="content">
+          <p>
+            <b>{user.name.first}</b> {user.name.last}
+          </p>
+          <p>Ciudad: {user.location.city} </p>
+          <p>Edad: {user.dob.age} años </p>
+        </div>
+      </div>
+    </li>
+  ));
+  return usersList;
+};
+
+export default Item;
